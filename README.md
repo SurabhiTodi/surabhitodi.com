32774faf35bd5a42ee26ed23ac534a525fddbd04
tatiaris <tatiaris2001@gmail.com>
2021-05-16 14:01:07 -0500

    first learner commit student

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# surabhitodi.com
-Personal Website
+This is a starter template for [Learn Next.js](https://nextjs.org/learn).
---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>About- Surabhi Todi</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div>
+        Hi my name is Surabhi Todi.
+        <p>
+          this is a paragrph
+        </p>
+        <p>My name is Danie=l. I work on <a href="/ubisucks">ubisucks</a>.</p>
+      </div>
+    </div>
+    
+  )
+}
