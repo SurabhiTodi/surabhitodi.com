05099c11b393ef52b34d7f4109f929a9eacc5bd9
Surbhi Todi <84286174+SurabhiTodi@users.noreply.github.com>
2021-06-12 13:32:16 -0500

    Update how-to-become-convincing.js

--- pages/posts/how-to-become-convincing.js
+++ pages/posts/how-to-become-convincing.js
@@ -5,7 +5,7 @@ import CFooter from '../../components/CFooter'
 export default function Article() {
   return (
     <div className="container">
-      <CHead title="How to be Convincing"/>
+      <CHead title="How To Become Convincing"/>
       <Navigation />
       <main className="article-container">
         <div className="article-meta-data">
@@ -14,18 +14,23 @@ export default function Article() {
         </div>
         <div className="divider"></div>
         1. Listen and WATCH- Start off with understanding what the person is saying and WHY. This is the most important part- if you don't know what the narrative is in the other persons mind you can't change it, after hearing them out ask questions to make sure they mean what they are saying and they are saying what they mean, use perception for body language cues.
-<br/>
+<br/> <br/>
+
 2. Repeat- Show that you understood their narrative- you can say something like so you mean?? This is the time to really have them feel heard and UNDERSTOOD- if they don't think you understood them you won't move forward from the supposed lack of understanding. Game over. At this point if you want to make sure that you really understood what they mean not just what they are saying is by asking the same questions but framed in different ways- what do you hate most about your current job might give you a different answer than why are you quitting your current job- but these questions are similar enough to understand if there is a discrepancy in what they are saying and what they mean. 
 After this you can do any of the following- sometimes multiple in parallel depending on the situation- do they already have a set idea that needs to be changed, is it a brand new idea, or are they started from a place of fixed mindset. 
 Reframe bad narratives- Once you have understood why they think the way they do you can challenge their assumptions, think of this like a Jenga game, you need to slide out the right pieces and slide in your own new pieces, eventually changing the entire structure or even the foundation. Start with easy to invalidate and after gaining credibility move to harder.
-<br/>
+<br/> <br/>
+
 3. Create New Stories- If you are convincing them to do something and they see it to have a certain outcome, try and weave a new story from the starting point. Maybe they are scared of water, try and make them think about how exciting it will be to try out the restaurant on board. You can't get rid of the fear of water but you can make them not be paranoid and focus on the other story instead.
-<br/>
+<br/><br/>
+
 
 4. Invalidate Assumptions- Narratives are often based on assumptions, you can use data, facts, etc, to show that the base assumption is wrong. If you can show that a basic assumption is wrong you can then insert your own story. breaking out of original narrative and into the direction you want to lead people. 
-<br/>
+<br/> <br/>
+
 
 5. Use Simple Language- People are not as impressed as you think they are when you say things they don't understand. This is lazy and ineffective.
+<br/>
 <br/>
 
 6. Be likeable- You are more likely to be convinced by someone you like and want to trust. Fairly simple and yet extremely important. If you struggle with this, this book by Dale Carnegie from 1936 should help. The advice is timeless and still applicable. 
