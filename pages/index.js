5375309c85dc570c95e2c34b0c5f736cf46a799b
Surbhi Todi <84286174+SurabhiTodi@users.noreply.github.com>
2021-06-12 13:56:45 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -7,9 +7,16 @@ export default function Home() {
         <title>Home | Surabhi Todi</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <p>My name is Surabhi Todi.</p>
-      <p>This is my website.</p>
-      <p>Learn more <a href="/about">about me</a>.</p>
+      <p>I'm a writer and marketer who likes to understand how people work. I try to engage in both theory and practice, which means I'm always cycling between writing to understand a topic, then finding ways to test those ideas in the "real world."
+
+I'm currently interested in memestock communities and meme-based economies. 
+
+Previously, I founded a social e-commerce site for pre-owned clothing called [Clothwrks.com](http://clothwrks.com), working to help solve the crises of fast fashion waste we currently face. Recently I have been consulting with startups on growth and product strategy, fundraising and achieving product market fit.
+
+I occasionally write on this website ([RSS](https://nadiaeghbal.com/feed.xml)), and I also publish my [notes](https://nadiaeghbal.com/notes/) every month. For future updates, you can sign up for my [monthly newsletter](https://nayafia.substack.com/), where I share my writing, TikTok videos, and anything interesting I've read.
+
+**If you'd like to get in touch**, find me on Twitter [@SurbhiTodi](https://twitter.com/SurbhiTodi) or email *surbhi1todi@gmail.com.* I typically respond the fastest to Twitter messages!.</p>
+      
       <p>Check out my <a href="/posts">posts</a>.</p>
     </div>
   )
