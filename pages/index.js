4ff14645b486e36665d11c9a9d993e045a0b4286
Surbhi Todi <84286174+SurabhiTodi@users.noreply.github.com>
2021-06-18 13:55:33 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
       <p>I'm a writer and marketer who likes to understand how people work. I try to engage in both theory and practice, which means I'm always cycling between writing to understand a topic, then finding ways to test those ideas in the "real world."
 
 I'm currently interested in meme-based economics- the ultimate form of marketing. I'm also interested in the attention economy- or how companies can profit off of people's attention.
-
+      <p>
 Previously, I founded a social e-commerce site for pre-owned clothing called [Clothwrks.com](http://clothwrks.com), working to help solve the crises of fast fashion waste we currently face. Recently I have been consulting with startups on growth and product strategy, fundraising and achieving product market fit.
 
 I occasionally write on this website. For future updates, you can sign up for my [monthly newsletter], where I share my writing, TikTok videos, and anything interesting I've read.
