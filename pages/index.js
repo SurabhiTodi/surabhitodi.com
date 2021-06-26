67f60fb3dca8520c3d544dafcd7efb85eb64d1e1
Surbhi Todi <84286174+SurabhiTodi@users.noreply.github.com>
2021-06-25 22:08:55 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -7,15 +7,15 @@ export default function Home() {
         <title>Home | Surabhi Todi</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <p>I'm a writer and marketer who likes to understand how people work. I try to engage in both theory and practice, which means I'm always cycling between writing to understand a topic, then finding ways to test those ideas in the "real world."
+      <p>I'm a writer and marketer who likes to understand how people work. I try to engage in both theory and practice, which means I'm always cycling between writing to understand a topic, then finding ways to test those ideas in the "real world."</p>
 
 I'm currently interested in meme-based economics- the ultimate form of marketing. I'm also interested in the attention economy- or how companies can profit off of people's attention.
-      <p>
-Previously, I founded a social e-commerce site for pre-owned clothing called [Clothwrks.com](http://clothwrks.com), working to help solve the crises of fast fashion waste we currently face. Recently I have been consulting with startups on growth and product strategy, fundraising and achieving product market fit.
+
+<p>Previously, I founded a social e-commerce site for pre-owned clothing called <a href= "http://clothwrks.com">, working to help solve the crises of fast fashion waste we currently face. Recently I have been consulting with startups on growth and product strategy, fundraising and achieving product market fit.
 
 I occasionally write on this website. For future updates, you can sign up for my [monthly newsletter], where I share my writing, TikTok videos, and anything interesting I've read.
 
-**If you'd like to get in touch**, find me on Twitter [@SurbhiTodi](https://twitter.com/SurbhiTodi) or email *surbhi1todi@gmail.com.* I typically respond the fastest to Twitter messages!.</p>
+**If you'd like to get in touch**, find me on Twitter <a href="https://twitter.com/SurbhiTodi">@SurbhiTodi</a> or email *surbhi1todi@gmail.com.* I typically respond the fastest to Twitter messages!.</p>
       
       <p>Check out my <a href="/posts">posts</a>.</p>
     </div>
